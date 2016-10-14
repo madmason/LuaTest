0cf79e74ddfc51b56a92cc5f7a2efa3f2ba01dc0
Sebastian <kiehne@ifsr.de>
2016-10-15 00:22:58 +0200

    Hello World in lua

---
+++ HelloWorld.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Sebastian
+-- Date: 14.10.2016
+-- Time: 21:11
+-- To change this template use File | Settings | File Templates.
+--
+
+print "Hello World!"
+
